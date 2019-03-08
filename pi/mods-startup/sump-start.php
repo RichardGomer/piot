f0ef2b14bd9e4cbf68109bde8d85857660de980b
Richard Gomer <richard@xebre.net>
2019-03-08 16:56:09 +0000

    Tweak parameters, add minimum period between pumping

--- pi/mods-startup/sump-start.php
+++ pi/mods-startup/sump-start.php
@@ -1,8 +1,10 @@
 <?php
 
 $hole_depth = 48; // Depth of hole (from sensor) in cm
-$pump_on = 12; // Pump comes on when this level is reached (in cm)
+$pump_on = 11; // Pump comes on when this level is reached (in cm)
 $pump_off = 5; // And goes off when this level is reached (in cm)
+
+$min_interval = 3600; // Minimum amount of time (s) between the start of pumping episodes
 
 // Obviously, pump_off must be lower than pump_on; and there should be a reasonable range
 // to avoid flip-flopping, and to ensure water is actually drained (instead of just going
@@ -13,6 +15,10 @@ $log = $MOD['logger'] = new Sump\SumpLog();
 $MOD['sensor'] = new Sump\DepthSensor(31, 33, $hole_depth);
 
 $MOD['pump_range'] = array($pump_on, $pump_off);
+
+$MOD['min_interval'] = $min_interval;
+
+$MOD['last_on'] = 0;
 
 $store = new \mods\JsonStore('sumpinfo');
 $store->on_level = $pump_on;
